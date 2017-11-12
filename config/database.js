a292cd145c6b7fe6cfad2e60044edef87d294b22
AndreaMoon <andrealuna2008@live.it>
2017-11-12 18:29:39 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  uri: 'mongodb://Andrea85:18061985@ds149855.mlab.com:49855/nodejs-task1',
+  uri: 'mongodb://*****:*****@ds149855.mlab.com:49855/nodejs-task1',
   db: 'nodejs-task1git '
 }
